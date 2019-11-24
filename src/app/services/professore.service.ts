b324ba6cbe7e73ed683092414d6386a2588f7603
Thiago Cunha <thigosantoscunha@outlook.com>
2019-11-24 12:28:44 -0300

    Criando serviço de professor

--- src/app/services/professore.service.ts
+++ src/app/services/professore.service.ts
@@ -1,8 +1,36 @@
+import { Observable } from 'rxjs';
+import { ProfessorModel } from './../core/models/professor.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProfessorService {
-  private baseUrl: string = environment.base_url + '/v1/matriculas/alunos';
+  private baseUrl: string = environment.base_url + '/v1/matriculas/professores';
 
   constructor(private http: HttpClient) { }
+
+  findAll(): Observable<ProfessorModel[]> {
+    return this.http.get<ProfessorModel[]>(this.baseUrl);
+  }
+
+  findById(id: number): Observable<ProfessorModel> {
+    return this.http.get<ProfessorModel>(this.baseUrl + `/${id}`);
+  }
+
+  insert(professor: ProfessorModel): Observable<ProfessorModel> {
+    console.log('endpoint cria professor', professor);
+    return this.http.post<ProfessorModel>(this.baseUrl, professor);
+  }
+
+  update(professor: ProfessorModel): Observable<ProfessorModel> {
+    console.log('endpoint atualiza professor', professor);
+    return this.http.put<ProfessorModel>(this.baseUrl, professor);
+  }
+
+  deleteById(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}?id=${id}`);
+  }
 }
