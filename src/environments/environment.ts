88c2ef9758817fb9d3b1875c061552dbdac45f08
Thiago Cunha <thiagosantoscunha@outlook.com>
2019-12-13 04:22:14 -0300

    Fix bug url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // base_url: 'https://cors-anywhere.herokuapp.com/https://ktrakapp.herokuapp.com',
-  base_url: 'http://localhost:8080',
+  base_url: 'https://cors-anywhere.herokuapp.com/https://ktrakapp.herokuapp.com',
+  // base_url: 'http://localhost:8080',
 };
 
 /*
